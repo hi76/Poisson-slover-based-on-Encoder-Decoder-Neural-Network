7edc82deae6d489529ca0a92d8aac547d94104da
hi76 <vip.lovey@qq.com>
2021-05-08 22:50:42 +0800

    del

--- operator.jl
+++
@@ -1,8 +0,0 @@
-using Images
-
-function _operator(a)
-    kernel = [0 0 0 ;0 1 0 ;0 0 0; 0 1 0; 1 -6 1; 0 1 0; 0 0 0 ;0 1 0 ;0 0 0]
-    kernel = reshape(kernel,3,3,3)
-    grad2 = imfilter(a, kernel,Fill(0))
-    return grad2
-end
